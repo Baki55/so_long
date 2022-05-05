de6185a6d5a51f63d064ef9c979f31c9bf44f4f8
baki <badrkhatib@hotmail.com>
2022-05-05 18:09:23 +0200

    some tests

---
+++ main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 16:35:52 by bkhatib           #+#    #+#             */
+/*   Updated: 2022/05/05 17:33:39 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+
+int	main(void)
+{
+	void	*mlx_ptr;
+	void	*window_ptr;
+	
+	mlx_ptr = mlx_init();
+	window_ptr = mlx_new_window(mlx_ptr, 512, 512, "SO_LONG");
+	mlx_loop(mlx_ptr);
+}
