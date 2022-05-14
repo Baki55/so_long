33a19eb15fc15d27d88d33e2af752df6a7e66432
Badreldine Khatib <bkhatib@fu-r2-p1.s19.be>
2022-05-14 13:03:27 +0200

    added some struct in header file

--- so_long.h
+++ so_long.h
@@ -6,7 +6,36 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 12:50:40 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/14 12:51:12 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/14 13:02:22 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "mlx.h"
+
+typedef struct s_vector
+{
+	int	x;
+	int	y;
+}				t_vector;
+
+// all info needed for the map
+typedef struct s_map
+{
+	char		**map;
+	int			col;
+	int			row;
+	int			collectibles;
+	t_vector	pos_player;
+}				t_map;
+
+typedef struct s_program
+{
+	void		*mlx_ptr;
+	void		*win_ptr;
+	void		*img_ptr;
+	int			img_size;
+	t_map		map;
+	int			fd;
+	int			can_exit;
+	size_t		mv_count;
+}				t_program;
