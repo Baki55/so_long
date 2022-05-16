1ba1c868bcdccafb475877ab618b980efe8c9468
Badreldine Khatib <bkhatib@fu-r3-p8.s19.be>
2022-05-16 16:57:43 +0200

    added fct check_map

--- parser/checker.c
+++ parser/checker.c
@@ -6,7 +6,15 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 15:00:49 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/13 15:14:22 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/14 13:08:13 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../so_long.h"
+
+void	check_map(char *file_name, t_program *game)
+{
+	if (check_extention(file_name, ".ber") == 0)
+		err_etension(file_name);
+	
+}
