38966283dfff69e7183f81fc1d3eb87580335edf
baki <badrkhatib@hotmail.com>
2022-05-21 23:38:17 +0200

    added map_len fct

---
+++ parser/map_len.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_len.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 23:11:08 by bkhatib           #+#    #+#             */
+/*   Updated: 2022/05/21 23:14:22 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+int	map_len(int fd)
+{
+	int	len;
+	char	*line;
+	
+	len = 0;
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		len++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	return (len);
+}
