92093ca8d871f543a2692e0a9dc9d5101c88dfac
baki <badrkhatib@hotmail.com>
2022-05-25 22:10:56 +0200

    maj so_long.h

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 12:50:40 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/22 22:34:47 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/25 22:00:58 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,9 +48,11 @@ typedef struct s_program
 
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *str);
+int	in_set(char c);
 char	*get_next_line(int fd);
 int	map_len(int fd);
 void	get_map(char *file_name, t_program *game);
 int	ft_strcmp(const char *s1, const char *s2);
 int	check_extention(const char *file_name, const char *extension);
+int	check_char(t_program game);
 void	err_extension();
