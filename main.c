cff6a24ce1b9536061377f68a037f0fea59c7458
baki <badrkhatib@hotmail.com>
2022-05-25 22:59:07 +0200

    test check_ecp

--- main.c
+++ main.c
@@ -6,17 +6,50 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 16:35:52 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/22 22:35:24 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/25 22:52:40 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <stdio.h>
 
+int	ft_strchr(char *str, char c)
+{
+	while (*str != '\0')
+	{
+		if (*str == c)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+int	check_ecp(t_program game)
+{
+	int	e;
+	int	c;
+	int	p;
+	int	i;
+
+	i = 0;
+	e = 0;
+	c = 0;
+	p = 0;
+	while (game.map.map[i])
+	{
+		if (ft_strchr(game.map.map[i], 'E') == 1)
+			e = 1;
+		if (ft_strchr(game.map.map[i], 'C') == 1)
+			c = 1;
+		if (ft_strchr(game.map.map[i], 'P') == 1)
+			p = 1;
+		i++;
+	}
+	return (e && c && p);
+}
 int	main(void)
 {
-	if (check_extention("maps/map1.ber", ".ber") != 0)
-		err_extension();
-	else
-		write(1, "OK", 2);
+	t_program	game;
+	get_map("maps/map1.ber", &game);
+	printf("%d", check_ecp(game));
 }
