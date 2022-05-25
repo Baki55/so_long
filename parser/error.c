44b694a48f46f7aafca5b2e8fa51add4a3bea4f4
baki <badrkhatib@hotmail.com>
2022-05-25 12:16:35 +0200

    added exit_failure

--- parser/error.c
+++ parser/error.c
@@ -6,13 +6,13 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 21:09:18 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/22 21:42:00 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/22 22:24:41 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-void	err_extension(char *file_name)
+void	err_extension()
 {
 	write(1, "Invalid file extension", 22);
 	exit(EXIT_FAILURE);
@@ -21,29 +21,35 @@ void	err_extension(char *file_name)
 void	err_char(char *file_name)
 {
 	write(1, "Invalid character found", 23);
+	exit(EXIT_FAILURE);
 }
 
 void	err_rectangular(t_program game)
 {
 	write(1, "The map isn't rectangular", 25);
+	exit(EXIT_FAILURE);
 }
 
 void	err_exit(t_program game)
 {
 	write(1, "There's no exit in the map", 26);
+	exit(EXIT_FAILURE);
 }
 
 void	err_collectible(t_program game)
 {
 	write(1, "There's no collectible in the map", 33);
+	exit(EXIT_FAILURE);
 }
 
 void	err_player(t_program game)
 {
 	write(1, "There's no player in the map", 28);
+	exit(EXIT_FAILURE);
 }
 
 void	err_closed(t_program game)
 {
 	write(1, "The map isn't closed", 20);
+	exit(EXIT_FAILURE);
 }
