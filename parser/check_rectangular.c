0cfe147c34f15557fd66732477646e46067b40d8
baki <badrkhatib@hotmail.com>
2022-05-25 22:23:48 +0200

    added check_rectangular.c file

---
+++ parser/check_rectangular.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_rectangular.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 22:11:26 by bkhatib           #+#    #+#             */
+/*   Updated: 2022/05/25 22:18:20 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+int	check_rectangular(t_program game)
+{
+	int	row;
+	int	len;
+	int	line_len;
+
+	row = 0;
+	line_len = ft_strlen(game.map.map[row]);
+	while (game.map.map[row] != NULL)
+	{
+		len = ft_strlen(game.map.map[row]);
+		if (line_len != len)
+			return (1);
+		row++;
+	}
+	return (0);
+}
