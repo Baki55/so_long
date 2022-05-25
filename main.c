ddfabc1058809acd45e4f3a0f7e9d6a073434d75
baki <badrkhatib@hotmail.com>
2022-05-25 12:17:55 +0200

    some tests

--- main.c
+++ main.c
@@ -6,24 +6,17 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 16:35:52 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/22 18:13:13 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/22 22:35:24 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
 #include "so_long.h"
 #include <stdio.h>
 
 int	main(void)
 {
-	t_program	game;
-	int	i;
-	
-	i = 0;
-	get_map("maps/map1.ber", &game);
-	while (i < game.map.row)
-	{
-		printf("%s\n", game.map.map[i]);
-		i++;	
-	}
+	if (check_extention("maps/map1.ber", ".ber") != 0)
+		err_extension();
+	else
+		write(1, "OK", 2);
 }
