9035b60054d5d36a8ce5128d935b9d02dd07a6d9
Badreldine Khatib <bkhatib@shi-r11-p2.s19.be>
2022-05-26 16:00:28 +0200

    added check_ecp.c file

---
+++ parser/check_ecp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_ecp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/26 15:53:41 by bkhatib           #+#    #+#             */
+/*   Updated: 2022/05/26 15:57:07 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+int	check_ecp(t_program game)
+{
+	int	e;
+	int	c;
+	int	p;
+	int	i;
+
+	i = 0;
+	e = 0;
+	c = 0;
+	p = 0;
+	while (game.map.map[i])
+	{
+		if (ft_strchr(game.map.map[i], 'E') == 1)
+			e = 1;
+		if (ft_strchr(game.map.map[i], 'C') == 1)
+			c = 1;
+		if (ft_strchr(game.map.map[i], 'P') == 1)
+			p = 1;
+		i++;
+	}
+	return (e && c && p);
+}
