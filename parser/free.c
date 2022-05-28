e4508674ec654c51c723022b6c53af9901c37bdb
baki <badrkhatib@hotmail.com>
2022-05-28 19:34:31 +0200

    added the free_game fct

--- parser/free.c
+++ parser/free.c
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 21:54:32 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/26 22:02:31 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/28 19:15:25 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,18 @@ void	free_map(t_program *game)
 	while (game->map.map[i])
 		free(game->map.map[i++]);
 	free(game->map.map);
+}
+
+void	free_game(t_program *game, char *msg)
+{
+	int	i;
+
+	i = 0;
+	free_map(game);
+	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+	if (ft_strcmp(msg, "BRAVO!") == 0)
+		write(1, "Bravo :)", 8);
+	else
+		write(1, "Quitting the game", 17);
+	exit(EXIT_SUCCESS);
 }
