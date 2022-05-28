826de7208e1aa8eab084170028dd11957dedbac6
baki <badrkhatib@hotmail.com>
2022-05-28 19:35:54 +0200

    maj error fct

--- parser/checker.c
+++ parser/checker.c
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 15:00:49 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/26 22:14:38 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/28 18:22:03 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,21 @@ void	check_map(char *file_name, t_program *game)
 	if (check_char(*game) == 1)
 	{
 		free_map(game);
-		err_char(game);
+		err_char();
 	}
 	if (check_rectangular(*game) == 1)
 	{
 		free_map(game);
-		err_rectangular(game);
+		err_rectangular();
 	}
 	if (check_ecp(*game) == 0)
 	{
 		free_map(game);
-		err_ecp(game);
+		err_ecp();
 	}
 	if (check_closed(*game) == 1)
 	{
 		free_map(game);
-		err_closed(game);
+		err_closed();
 	}
 }
