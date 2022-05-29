4bdb3377d33a41b93e142ba7e97a2347b52c3586
baki <badrkhatib@hotmail.com>
2022-05-29 02:43:08 +0200

    some test for the init_game fct and the adding of the left/right moves

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 16:35:52 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/29 01:51:52 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/29 02:06:37 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,7 @@ int	main(void)
 	t_program	game;
 
 	check_map("maps/map3.ber", &game);
-	get_collectible(&game);
-	get_player_position(&game);
-	game.mv_count = 0;
-	game.mlx_ptr = mlx_init();
-	game.win_ptr = mlx_new_window(game.mlx_ptr, ft_strlen(game.map.map[0]) * 32, game.map.row * 32, "SO_LONG");
+	init_game(&game);
 	mlx_hook(game.win_ptr, 17, 0, close_window, &game);
 	render_images(game);
 	mlx_hook(game.win_ptr, 2, 0, *input_player, &game);
