1309b6719c48956267ce0eda784070ba46573578
baki <badrkhatib@hotmail.com>
2022-05-29 02:00:24 +0200

    added the itoa fct

--- parser/utils.c
+++ parser/utils.c
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 22:10:01 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/28 20:08:19 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/29 01:12:56 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,4 +124,55 @@ int	in_set(char c)
 			i++;
 	}
 	return (1);
+}
+
+static size_t	len_nbr(int n)
+{
+	size_t	nbr;
+
+	nbr = 0;
+	if (n == 0)
+		nbr++;
+	if (n == -2147483648)
+		n++;
+	if (n < 0)
+	{
+		n *= -1;
+		nbr++;
+	}
+	while (n > 0)
+	{
+		nbr++;
+		n /= 10;
+	}
+	return (nbr);
+}
+
+char	*ft_itoa(int c)
+{
+	size_t			len;
+	char			*buff;
+	unsigned int	n;
+
+	len = len_nbr(c);
+	if (c < 0)
+		n = (unsigned int)(c * -1);
+	else
+		n = (unsigned int)c;
+	buff = (char *)malloc(sizeof(char) * (len + 1));
+	if (buff != NULL)
+	{
+		if (c == 0)
+			buff[0] = '0';
+		if (c < 0)
+			buff[0] = '-';
+		buff[len] = '\0';
+		while (n > 0)
+		{
+			len--;
+			buff[len] = (char)(n % 10 + '0');
+			n /= 10;
+		}
+	}
+	return (buff);
 }
