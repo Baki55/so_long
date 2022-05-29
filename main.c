9446919dd0ccc2b7ca8fa2a9394831249a7b98f4
baki <badrkhatib@hotmail.com>
2022-05-29 18:30:34 +0200

    some tests with the moves

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 16:35:52 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/29 02:06:37 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/29 18:16:28 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int	main(void)
 
 	t_program	game;
 
-	check_map("maps/map3.ber", &game);
+	check_map("maps/map1.ber", &game);
 	init_game(&game);
-	mlx_hook(game.win_ptr, 17, 0, close_window, &game);
+	mlx_hook(game.win_ptr, 17, 0, *close_window, &game);
 	render_images(game);
 	mlx_hook(game.win_ptr, 2, 0, *input_player, &game);
 	mlx_loop(game.mlx_ptr);
