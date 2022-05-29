45fc0a0ef783a9dfcca72277b3cd3bd03022cfe4
Badreldine Khatib <bkhatib@shi-r11-p1.s19.be>
2022-05-29 14:26:12 +0200

    added str var for the mv_count to fix the leaks in itoa fct

--- rendering/render.c
+++ rendering/render.c
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 23:42:22 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/29 02:35:28 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/29 14:22:37 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ char	*get_image(char c, int can_exit, int mv_right)
 void	render_images_action(t_program game, int x, int y)
 {
 	char	*img_path;
+	char	*str;
 	
 	if (game.map.collectibles == 0)
 		game.can_exit = 1;
@@ -53,7 +54,9 @@ void	render_images_action(t_program game, int x, int y)
 				&game.img_size, &game.img_size);
 		mlx_put_image_to_window(game.mlx_ptr, game.win_ptr, game.img_ptr,
 			y * 32, x * 32);
-		mlx_string_put(game.mlx_ptr, game.win_ptr, 5, 15, 0xff0000, ft_itoa(game.mv_count));
+		str = ft_itoa(game.mv_count);
+		mlx_string_put(game.mlx_ptr, game.win_ptr, 5, 15, 0xff0000, str);
+		free(str);
 		mlx_destroy_image(game.mlx_ptr, game.img_ptr);
 	}
 }
