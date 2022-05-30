77ac90fa08dfd6d39a5e2e32339c737fa3fb4e37
baki <badrkhatib@hotmail.com>
2022-05-30 22:54:56 +0200

    added argc and argv in the main to be able to take the map in the terminal

--- main.c
+++ main.c
@@ -6,18 +6,22 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 16:35:52 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/30 14:17:09 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/30 22:27:53 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-
 	t_program	game;
-
-	check_map("maps/map2.ber", &game);
+	
+	if (argc != 2)
+	{
+		write(1, "Usage: ./so_long maps/[name_of_the_map].ber\n", 44);
+		exit(EXIT_FAILURE);
+	}
+	check_map(argv[1], &game);
 	init_game(&game);
 	mlx_hook(game.win_ptr, 17, 0, *close_window, &game);
 	render_images(game);
