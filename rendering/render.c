cf99ba9430aba70bfc0e449cc38cbed63039b2d8
Badreldine Khatib <bkhatib@shi-r10-p4.s19.be>
2022-05-30 14:28:59 +0200

    deleted the printing in the window

--- rendering/render.c
+++ rendering/render.c
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 23:42:22 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/29 17:58:15 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/30 14:17:17 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ char	*get_image(char c, int can_exit, int mv_right)
 void	render_images_action(t_program game, int x, int y)
 {
 	char	*img_path;
-	char	*mv_count_str;
+	// char	*mv_count_str;
 	
 	if (game.map.collectibles == 0)
 		game.can_exit = 1;
@@ -54,9 +54,9 @@ void	render_images_action(t_program game, int x, int y)
 				&game.img_size, &game.img_size);
 		mlx_put_image_to_window(game.mlx_ptr, game.win_ptr, game.img_ptr,
 			y * 32, x * 32);
-		mv_count_str = ft_itoa(game.mv_count);
-		mlx_string_put(game.mlx_ptr, game.win_ptr, 5, 15, 0xff0000, mv_count_str);
-		free(mv_count_str);
+		// mv_count_str = ft_itoa(game.mv_count);
+		// mlx_string_put(game.mlx_ptr, game.win_ptr, 5, 15, 0xff0000, mv_count_str);
+		// free(mv_count_str);
 		mlx_destroy_image(game.mlx_ptr, game.img_ptr);
 	}
 }
