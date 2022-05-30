a203eec1d654e121de96f8567f3b13ee8536acea
Badreldine Khatib <bkhatib@shi-r10-p4.s19.be>
2022-05-30 13:56:01 +0200

    added the error_player fct for the check_player fct

--- parser/error.c
+++ parser/error.c
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 21:09:18 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/29 18:09:40 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/30 13:46:59 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,12 @@ void	err_closed()
 {
 	write(1, "Error\n", 6);
 	write(1, "The map isn't closed.\n", 22);
+	exit(EXIT_FAILURE);
+}
+
+void	err_player()
+{
+	write(1, "Error\n", 6);
+	write(1, "More than one player.\n", 21);
 	exit(EXIT_FAILURE);
 }
